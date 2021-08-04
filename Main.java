5a7041a09c806e243cb48d700d042b3f30987595
ericromario <83571203+ericromario@users.noreply.github.com>
2021-08-04 10:40:38 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
 	public static void main (String[] args){
     	System.out.println("Hello, Eric du courage!");
 	System.out.println(" Merci Seigneur!");
+	System.out.println(" Merci pour la protection!");
 	
     }
 
